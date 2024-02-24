3b089baeb5ef15e9256a23ab7de73e85a2251526
Kirill Dmitruk <dmitruk.ks@mail.ru>
2024-02-24 14:35:02 +0300

    Added data to tables

---
+++ homework-1/config.py
@@ -0,0 +1,7 @@
+import os
+
+
+data_path = os.path.dirname(os.path.dirname(__file__))
+customers_file = os.path.join(data_path, "homework-1", "north_data", "customers_data.csv")
+employees_file = os.path.join(data_path, "homework-1", "north_data", "employees_data.csv")
+orders_file = os.path.join(data_path, "homework-1", "north_data", "orders_data.csv")
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,20 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import pandas as pd
+import psycopg2
+from config import customers_file, employees_file, orders_file
+
+customers = pd.read_csv(customers_file)
+employees = pd.read_csv(employees_file)
+orders = pd.read_csv(orders_file)
+
+with psycopg2.connect(host="localhost", database="north", user="postgres", password="13Keb-09") as conn:
+    with conn.cursor() as cursor:
+        for customer in customers.itertuples(index=False):
+            cursor.execute("INSERT INTO customers VALUES (%s, %s, %s)", customer)
+
+        for employee in employees.itertuples(index=False):
+            cursor.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", employee)
+
+        for order in orders.itertuples(index=False):
+            cursor.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", order)
+
+conn.close()
